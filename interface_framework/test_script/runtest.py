9e2f9b45259f52ceefa9d23ecf40e37a001f03bd
echo6120 <jingyu@youdao.internal>
2018-07-11 20:06:41 +0800

    modify path

--- interface_framework/test_script/runtest.py
+++ interface_framework/test_script/runtest.py
@@ -11,7 +11,8 @@ import os
 curPath = os.path.abspath(os.path.dirname(__file__))
 rootPath = os.path.split(curPath)[0]
 sys.path.append(rootPath)
-
+sys.path.append("D:\\test\\python\\interface_framework\\Action")
+sys.path.append("D:\\test\\python\\interface_framework\\Util")
 class RunTest:
 	def __init__(self):
 		#模拟post/get请求
